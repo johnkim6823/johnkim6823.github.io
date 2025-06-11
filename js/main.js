f0c6286b7abd3f7c7b4462225d934aa83c3898de
Myeongjun Kim <johnkim682@gmail.com>
2025-06-11 18:11:25 +0900

    Update awards section to static HTML

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-const awards = [
-  { year: 2025, date: '2025.3', desc: 'detail', org: 'detail' },
-  { year: 2024, date: '2024.3', desc: 'detail', org: 'detail' },
-  { year: 2023, date: '2023.3', desc: 'detail', org: 'detail' },
-  { year: 2022, date: '2022.3', desc: 'detail', org: 'detail' },
-  { year: 2021, date: '2021.3', desc: 'detail', org: 'detail' },
-  { year: 2020, date: '2020.3', desc: 'detail', org: 'detail' },
-  { year: 2018, date: '2018.3', desc: 'detail', org: 'detail' },
-  { year: 2017, date: '2017.3', desc: '1번', org: 'Baekseok University' },
-  { year: 2017, date: '2017.3', desc: '2번', org: 'Baekseok University' },
-  { year: 2017, date: '2017.9', desc: '3번', org: 'Baekseok University' }
-];
-
 const aboutInfo = [
   'Information coming soon...'
 ];
@@ -42,19 +29,6 @@ function ProjectsList({ projects }) {
   );
 }
 
-function AwardsList({ awards }) {
-  return (
-    <>
-      {awards.map((a, i) => (
-        <div className="award-entry" key={i}>
-          <div className="award-year">{a.year}<small>{a.date}</small></div>
-          <div className="award-desc">{a.desc}</div>
-          <div className="award-org">{a.org}</div>
-        </div>
-      ))}
-    </>
-  );
-}
 
 ReactDOM.render(
   <About info={aboutInfo} />, 
@@ -66,9 +40,5 @@ ReactDOM.render(
   document.getElementById('projects-root')
 );
 
-ReactDOM.render(
-  <AwardsList awards={awards} />,
-  document.getElementById('awards-root')
-);
 
 console.log('Portfolio loaded with React');
